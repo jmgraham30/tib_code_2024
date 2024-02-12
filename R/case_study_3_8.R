24b9270d51f6fae4953bc38868c7e55955562b38
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-02-12 08:04:45 -0500

    update

--- R/case_study_3_8.R
+++ R/case_study_3_8.R
@@ -6,9 +6,11 @@ c_val <- 10
 ggplot(data.frame(u = c(0,20)), aes(x = u)) +
   geom_function(fun = function(u){u / (1 + u^2)},color="steelblue",linewidth=1) +
   geom_function(fun = function(u){0.1*(1 - u/c_val)},color="green",linetype="dashed",linewidth=1) +
-  geom_function(fun = function(u){0.25*(1 - u/c_val)},color="green",linetype="dashed",linewidth=1) +
-  geom_function(fun = function(u){0.45*(1 - u/c_val)},color="green",linetype="dashed",linewidth=1) +
-  geom_function(fun = function(u){0.56*(1 - u/c_val)},color="green",linetype="dashed",linewidth=1) +
-  geom_function(fun = function(u){0.7*(1 - u/c_val)},color="green",linetype="dashed",linewidth=1) +
+  geom_function(fun = function(u){0.25*(1 - u/c_val)},color="orange",linetype="dashed",linewidth=1) +
+  geom_function(fun = function(u){0.37*(1 - u/c_val)},color="purple",linetype="dashed",linewidth=1) +
+  geom_function(fun = function(u){0.45*(1 - u/c_val)},color="yellow",linetype="dashed",linewidth=1) +
+  geom_function(fun = function(u){0.56*(1 - u/c_val)},color="purple",linetype="dashed",linewidth=1) +
+  geom_function(fun = function(u){0.7*(1 - u/c_val)},color="brown",linetype="dashed",linewidth=1) +
   labs(x = "u", y = "", title = TeX("Plot of $\\frac{u}{1+u^2}$ and $h(1 - \\frac{u}{c})$")) + 
   ylim(c(-0.05,0.75))
+
