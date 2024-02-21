1926b36871abe51f54027c1906d6bf65b6bcb547
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-02-21 08:50:13 -0500

    update

--- R/turelli_app/ui.R
+++ R/turelli_app/ui.R
@@ -3,11 +3,11 @@ ui <- fluidPage(
   titlePanel("Wolbachia Infection Frequency Model"),
   sidebarLayout(
     sidebarPanel(
-      numericInput("p0", "Initial Value (p0):", value = 0.4),
-      numericInput("N", "Number of Time Steps (N):", value = 100),
-      numericInput("F_val", "Parameter F:", value = 0.5),
-      numericInput("mu_val", "Parameter mu:", value = 0.5),
-      numericInput("s_h", "Parameter s_h:", value = 0.0)
+      numericInput("p0", "Initial Value (p0):", value = 0.4, min = 0, max = 1),
+      numericInput("N", "Number of Time Steps (N):", value = 100, min=1,max=1000),
+      numericInput("F_val", "Parameter F:", value = 0.5,min=0.0,max=2.0),
+      numericInput("mu_val", "Parameter mu:", value = 0.5,min=0.0,max=1.0),
+      numericInput("s_h", "Parameter s_h:", value = 0.0,min=0.0,max=1.0)
     ),
     mainPanel(
       plotOutput("dynamicalPlot")
